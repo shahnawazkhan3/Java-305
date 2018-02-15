2bc1b064d6ce67fee9a8f3e5371f3bd4104c42c3
shahnawazGold <shahnawazkhan_333@yahoo.com>
2018-02-15 13:46:10 +0500

    1st commit

---
+++ Java_305_12_2_18/src/Operators.java
@@ -0,0 +1,95 @@
+
+public class Operators {
+	
+	
+
+	public static void main(String[] args) {
+		
+		// TODO Auto-generated method stub
+		
+		
+		///Arithmetic Operators
+		
+		
+//		int a =2;
+//		int b =4;
+//		
+//		int c = (a+ b);
+//		int d =(a-b);
+//		int e =(a*b);
+//		int f =(a /b);
+//		
+//		int g= (a++); 
+//		
+//		System.out.println(" a + b " +c);
+//		System.out.println(" a - b " +d);
+//		System.out.println(" a * b " +e);
+//		System.out.println(" a / b " +f);
+		//System.out.println(" a a++ b " +c);
+		
+		
+		/////Relational Operators////////
+		
+		
+//		int a = 2;
+//		int b = 4;
+//		
+//		
+//		System.out.println(" a ==b =" +(a ==b ));
+//		System.out.println(" a != b =" + (a!=b));
+//		
+//		System.out.println(" a > b =" + (a >b));
+//		System.out.println(" a < b =" + (a <b));
+//		
+//		System.out.println(" a <= b =" + (a <=b));
+//		
+//		System.out.println(" a >= b =" + (a >=b));
+//		
+		
+		
+		
+//		  The Logical Operators
+//		
+		
+//		boolean a = true;
+//		boolean b = false;
+//		
+//		/// OR , AND //
+//		
+//		/// AND////
+//		
+//		System.out.println(" a &&  b =" +(a && b));
+//		
+//		/// OR 
+//		
+//		System.out.println(" a || b "+ (a|| b));
+//		
+//	/// AND////
+//		
+//		System.out.println(" a &&  b =" +!(a && b));
+		
+		
+		
+		/// The Assignment Operators.///////////////////////////////
+		
+		int a = 2;
+		int b =4;
+		
+		int c=a+b;
+		System.out.println("c= a+b"+c);
+		c+=a;
+		System.out.println(" C+="+c);
+		c-=a;
+		System.out.println(" C-="+c);
+		c--;
+		
+		System.out.println(" C*="+c);
+		
+		
+		
+		
+		
+
+	}
+
+}
