4e6d96e06ad27e8b1301938c1ad2ef3bf0647684
shahnawazGold <shahnawazkhan_333@yahoo.com>
2018-02-17 13:08:52 +0500

    17/2/18

---
+++ Alpha_String_16_2_18/src/Stringss.java
@@ -0,0 +1,87 @@
+
+public class Stringss {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("wellcome yes");
+     
+     
+      
+     //=================String charAt() Method==============
+     
+     
+     
+//     String st = "wellcom java Student";
+//     
+//      char result=st.charAt(8);
+//      
+//      System.out.println(" result = "+result);
+     
+     
+     //==============String compareTo( o object) Method==============
+     
+//      String st1 = "Strings are immutable";
+//      String st4="shah";
+//      
+//      String st2 =new String("Strings are immutable");
+//      String st3 =new String("Strings are not immutable");
+//      
+//      int result =st1.compareTo(st4);
+//      
+//      System.out.println(result);
+//      
+//      
+//      result = st2.compareTo( st3 );
+//      System.out.println(result);
+     
+     
+     //==================String compareTo(String anotherString)=============
+     
+     
+//     String st1 = "Strings are immutable";
+//     String st2 = "Strings are immutable";
+//     String st3 = "Integers are not immutable";
+//     
+//     
+//     int result = st1.compareTo(st2);
+//     
+//     System.out.println(result);
+//     
+//     result =st2.compareTo(st3);
+//     
+//     System.out.println(result);
+//     
+//     result=st3.compareTo(st1);
+//     
+//     System.out.println(result);
+//     
+     
+        //=================String compareToIgnoreCase() Method========
+     
+     
+//        String st1 ="Strings are immutable";
+//        String st2 = "Strings are immutable";
+//        String st3 ="Strings are not immutable";
+//        
+//        int result = st1.compareToIgnoreCase(st2);
+//        System.out.println(result);
+//        
+//        result = st2.compareToIgnoreCase(st3);
+//       
+//        System.out.println(result);
+//        
+//        result= st3.compareToIgnoreCase(st1);
+//        System.out.println(result);
+        
+        //===========Java - String concat() Method============
+     
+     
+        String name = "shah";
+        
+        name=name.concat("  khan");
+        
+        System.out.println(name);
+	}
+	
+
+}
