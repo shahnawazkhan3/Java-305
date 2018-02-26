632db7f642c213984546cc818381d1ebbed1989f
shahnawazGold <shahnawazkhan_333@yahoo.com>
2018-02-26 12:51:20 +0500

    23/2/18 number

---
+++ 305Number_23_2_18/src/Numbers.java
@@ -0,0 +1,53 @@
+
+public class Numbers {
+	
+
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      
+		
+		//================Java - xxxValue() Method=========
+		
+//		Number x =5;
+//		
+//		System.out.println(x.doubleValue());
+//		System.out.println(x.byteValue());
+//		System.out.println(x.toString());
+		
+		//============Java - compareTo() Method
+		
+//		Integer x = 5;
+//		
+//		System.out.println(x.compareTo(3));
+//		System.out.println(x.compareTo(8));
+		
+		
+		//==========Java - equals() Method
+//		
+//		Integer x = 5;
+//		Integer y =5;
+//		
+//		System.out.println(x.equals(y));
+		
+		//===============
+		
+//		Integer a = Integer.valueOf(5);
+//		//Double b = Double.valueOf(6);
+//		Integer b = Integer.valueOf("7",16);
+//		
+//		System.out.println(b);
+		
+		int x =Integer.parseInt("9");
+		double c = Double.parseDouble("5");
+		int b = Integer.parseInt("444",16);
+		System.out.println(x);
+		System.out.println(c);
+		System.out.println(b);
+		
+		
+	}
+
+}
